ff118ff05b91a4d3632941b57e454a7e92fede27
ronjahoo <ronjaheikkinen00@gmail.com>
2026-08-27 16:20:09 +0300

    Osa 1 tehtävä 1.13

--- OSA1/anekdootit/src/App.jsx
+++ OSA1/anekdootit/src/App.jsx
@@ -13,17 +13,27 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
 
-  const handleClick = () => {
+  const handleNext = () => {
     const randomIndex = Math.floor(Math.random() * anecdotes.length)
     setSelected(randomIndex)
+  }
+
+  const handleVote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
   }
 
   return (
     <div>
       {anecdotes[selected]}
       <br />
-      <button onClick={handleClick}>next anecdote</button>
+      has {votes[selected]} votes
+      <br />
+      <button onClick={handleVote}>vote</button>
+      <button onClick={handleNext}>next anecdote</button>
     </div>
   )
 }
