28a58f318cbc4c11e28f6358898f788fcb24a218
ronjahoo <ronjaheikkinen00@gmail.com>
2026-08-27 14:29:03 +0300

    Osa 1.A tehtävä 1.2

--- OSA1/1.1/tehtava-1.1/src/App.jsx
+++ OSA1/1.1/tehtava-1.1/src/App.jsx
@@ -18,17 +18,19 @@ const App = () => {
 
 const Content = (props) => {
   return (
-    <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
-    </>
+    <div>
+      <Part name={props.part1} exercises={props.exercises1} />
+      <Part name={props.part2} exercises={props.exercises2} />
+      <Part name={props.part3} exercises={props.exercises3} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
   )
 }
 
