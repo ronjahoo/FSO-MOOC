b6918d21c088d9cbf1e616b5acc7937c52f17427
ronjahoo <ronjaheikkinen00@gmail.com>
2026-08-28 06:29:46 +0300

    Osa 2 tehtävä 2.1

---
+++ OSA2/kurssitiedot/src/App.jsx
@@ -0,0 +1,64 @@
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+const Course = (props) => {
+  return (
+    <div>
+      <Header course={props.course} />
+      <Content parts={props.course.parts} />
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map(part =>
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      )}
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <h1>{props.course.name}</h1>
+  )
+}
+
+export default App
